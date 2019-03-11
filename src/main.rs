de2a04488d5ec0c4025895a61c532baa1ad3f968
Ron Georgia <rong1611@gmail.com>
2019-03-11 11:26:08 -0400

    Added usage_help.rs to hold usage and help messages.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod usage_help ;
+
 fn main() {
     println!("Hello, world!");
+	usage_help::print_help() ;
+	usage_help::print_usage() ;
+
 }
---
+++ src/usage_help.rs
@@ -0,0 +1,7 @@
+pub fn print_help() {
+	println!("This is the help contents")
+}
+
+pub fn print_usage() {
+	println!("This is usage content.")
+}
