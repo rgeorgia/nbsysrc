68e0cf834d032a3815c71e3ee2e7b058d0b87c9b
Ron Georgia <rong1611@gmail.com>
2019-03-11 11:40:37 -0400

    Playing with whoami library

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 mod usage_help ;
+extern crate whoami ;
 
 fn main() {
     println!("Hello, world!");
 	usage_help::print_help() ;
 	usage_help::print_usage() ;
+
+	println!("Operating System is: {}", whoami::os()) ;
+	//println!("{}", whoami::computer()) ;
+	println!("The environment is: {}", whoami::env()) ;
+	println!("You are on host: {}", whoami::hostname()) ;
+	println!("Your username is: {}", whoami::username()) ;
 
 }
