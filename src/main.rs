f9891abcecac0e1a3a477c6e983a7085425d77da
Ron Georgia <rong1611@gmail.com>
2019-03-11 12:11:02 -0400

    Playing with os_type library

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use os_type ;
 mod usage_help ;
+
 extern crate whoami ;
 
 fn main() {
+	let os = os_type::current_platform() ;
+
     println!("Hello, world!");
 	usage_help::print_help() ;
 	usage_help::print_usage() ;
 
 	println!("Operating System is: {}", whoami::os()) ;
-	//println!("{}", whoami::computer()) ;
+	println!("Operating System for os_type: {:?}", os.os_type) ;
+	println!("Operating System Version: {:?}", os.version) ;
+
 	println!("The environment is: {}", whoami::env()) ;
 	println!("You are on host: {}", whoami::hostname()) ;
 	println!("Your username is: {}", whoami::username()) ;
