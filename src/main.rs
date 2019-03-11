807dd89fadbd43b98eb57ce90d8f2c6bb237b830
Ron Georgia <ronverbs@gmail.com>
2019-03-11 12:32:46 -0400

    Removed a crate in favor of something that was more netbsd friendly

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,18 @@
 use os_type ;
+use std::env::consts::OS ;
 mod usage_help ;
 
 extern crate whoami ;
 
 fn main() {
-	let os = os_type::current_platform() ;
+	//let os = os_type::current_platform() ;
 
     println!("Hello, world!");
 	usage_help::print_help() ;
 	usage_help::print_usage() ;
 
 	println!("Operating System is: {}", whoami::os()) ;
-	println!("Operating System for os_type: {:?}", os.os_type) ;
-	println!("Operating System Version: {:?}", os.version) ;
-
+	println!("Operating System for platforms: {:?}", std::env::consts::OS) ;
 	println!("The environment is: {}", whoami::env()) ;
 	println!("You are on host: {}", whoami::hostname()) ;
 	println!("Your username is: {}", whoami::username()) ;
