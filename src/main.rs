65a719a5ae9bd552f73534b3ddf003ab8b3e381f
Ron Georgia <ronverbs@gmail.com>
2019-03-11 20:33:40 -0400

    Updated Cargo

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,7 @@ fn main() {
 	usage_help::print_help() ;
 	usage_help::print_usage() ;
 
-	println!("Operating System is: {}", whoami::os()) ;
-	println!("Operating System for platforms: {:?}", std::env::consts::OS) ;
-	println!("The environment is: {}", whoami::env()) ;
+	println!("Operating System: {}", std::env::consts::OS) ;
 	println!("You are on host: {}", whoami::hostname()) ;
 	println!("Your username is: {}", whoami::username()) ;
 
