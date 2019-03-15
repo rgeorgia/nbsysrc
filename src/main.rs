c34d96f212fc4656fb0f7b46401948e1dbedc330
Ron Georgia <rong1611@gmail.com>
2019-03-15 10:43:21 -0400

    Discovered clapgit statusgit status

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,13 @@
-use std::env;
-use std::io::{self, Write};
-use std::net::{IpAddr, TcpStream};
-use std::str::FromStr;
-use std::process;
+extern crate whoami ;
+extern crate clap ;
 
+use std::env ;
+use std::io::{Write} ;
+use std::str::FromStr ;
+use std::process ;
+use clap::{Arg, App, SubCommand} ;
 
 mod usage_help ;
-
-extern crate whoami ;
-
- struct Arguments {
-     key_value: String,
-	 list: String,
-	 test_dir: String,
-	 list_rc: bool,
-	 list_all: bool,
-	 show_rc: bool,
-	 interactive:bool,    
- }
-
-impl Arguments {
-    fn new(args: &[String]) -> Result<Arguments, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        } else if args.len() > 4 {
-            return Err("too many arguments");
-        } else {
-			println!("There are {} arguments. {:?}", args.len(), args) ;
-			return Err("something is wrong") ;
-		}
-
-    }
-}
-
 
 fn main() {
 
@@ -43,19 +18,20 @@ fn main() {
 	// println!("The environment is: {}", whoami::env()) ;
 	// println!("You are on host: {}", whoami::hostname()) ;
 	// println!("Your username is: {}", whoami::username()) ;
-
-	let args: Vec<String> = env::args().collect();
-	let program = args[0].clone();
-	let arguments = Arguments::new(&args).unwrap_or_else(
-		|err| {
-			if err.contains("help") {
-				process::exit(0);
-			} else {
-				eprintln!("{} problem parsing arguments: {}", program, err);
-				process::exit(0);
-			}
-		}
-	); //END let arguments
-
+	let matches = App::new("nbsysrc")
+                          .version("1.0")
+                          .author("Ron Georgia <netverbs@gmail.com>")
+                          .about("Simple sysrc for NetBSD")
+                          .args_from_usage(
+                              "-c, --config=[FILE] 'Sets a custom config file'
+                              <INPUT>              'Sets the input file to use'
+                              -v...                'Sets the level of verbosity'")
+                          .subcommand(SubCommand::with_name("test")
+                                      .about("controls testing features")
+                                      .version("1.3")
+                                      .author("Someone E. <someone_else@other.com>")
+                                      .arg_from_usage("-d, --debug 'Print debug information'"))
+                          .get_matches(); 
+	
 
 } //END
