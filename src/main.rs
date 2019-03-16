48da78bf0feed7b597b826c12d3e606ca7cc26f7
Ron Georgia <rong1611@gmail.com>
2019-03-16 09:36:17 -0400

    Changed dependancy to version number rather than *

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate whoami ;
+#[macro_use]
 extern crate clap ;
 
 use std::env ;
@@ -17,21 +18,6 @@ fn main() {
 	// println!("Operating System for platforms: {:?}", std::env::consts::OS) ;
 	// println!("The environment is: {}", whoami::env()) ;
 	// println!("You are on host: {}", whoami::hostname()) ;
-	// println!("Your username is: {}", whoami::username()) ;
-	let matches = App::new("nbsysrc")
-                          .version("1.0")
-                          .author("Ron Georgia <netverbs@gmail.com>")
-                          .about("Simple sysrc for NetBSD")
-                          .args_from_usage(
-                              "-c, --config=[FILE] 'Sets a custom config file'
-                              <INPUT>              'Sets the input file to use'
-                              -v...                'Sets the level of verbosity'")
-                          .subcommand(SubCommand::with_name("test")
-                                      .about("controls testing features")
-                                      .version("1.3")
-                                      .author("Someone E. <someone_else@other.com>")
-                                      .arg_from_usage("-d, --debug 'Print debug information'"))
-                          .get_matches(); 
-	
+	println!("Your username is: {}", whoami::username()) ;
 
 } //END
