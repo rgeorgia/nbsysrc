1c2ccb83ca4c7e24e4abeb4d4656bfac61ba3397
Ron Georgia <rong1611@gmail.com>
2019-03-16 22:27:35 -0400

    Adding and testing args with clap

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,34 @@ extern crate whoami ;
 #[macro_use]
 extern crate clap ;
 
-use std::env ;
-use std::io::{Write} ;
-use std::str::FromStr ;
-use std::process ;
 use clap::{Arg, App, SubCommand} ;
 
-mod usage_help ;
-
 fn main() {
+	let mut enabling = true  ;
 
-	// usage_help::print_help() ;
-	// usage_help::print_usage() ;
-	// println!("Operating System is: {}", whoami::os()) ;
-	// println!("Operating System for platforms: {:?}", std::env::consts::OS) ;
-	// println!("The environment is: {}", whoami::env()) ;
-	// println!("You are on host: {}", whoami::hostname()) ;
-	println!("Your username is: {}", whoami::username()) ;
+	let matches = App::new("nbsysrc")
+    	.about("does awesome things")
+       	.author(crate_authors!())
+	   	.arg(Arg::with_name("service")
+	   		.help("Key=value pair. Ex dbus=YES")
+	   		.required(true))
+       .get_matches() ;
 
+	if matches.value_of("service").unwrap().contains(&"flag") {
+		enabling = false ;
+		println!("You selected a flag type entry, {} {}",  matches.value_of("service").unwrap(), enabling) ;
+		
+	} else {
+		println!("You selecte a service type entry, {} {}", matches.value_of("service").unwrap(), enabling)
+		// enabling = true ;
+	}
+	profile() ;
 } //END
+
+fn profile() {
+	println!("Operating System is: {}", whoami::os()) ;
+	println!("Operating System for platforms: {:?}", std::env::consts::OS) ;
+	println!("The environment is: {}", whoami::env()) ;
+	println!("You are on host: {}", whoami::hostname()) ;
+	println!("Your username is: {}", whoami::username()) ;
+}
